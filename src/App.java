dcbdec820d63ba60bcb984f404901b1dd515c463
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-12-11 09:08:55 +0100

    klar med uppgift a

--- src/App.java
+++ src/App.java
@@ -3,6 +3,21 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner tb = new Scanner(System.in);
+        System.out.print("Skriv in ditt användarnamn: ");
+        String user = tb.nextLine();
+        System.out.print("Skriv in ditt lösenord: ");
+        String passwrd = tb.nextLine();
         
+
+        if (user.equals("root") && passwrd.equals("pass")) {
+            System.out.println("Ditt bankkonto har 0 kr");
+        }
+        else{
+            System.out.println("Fel användarnamn");
+        }
+        }
+
     }
-}
+
+
+
